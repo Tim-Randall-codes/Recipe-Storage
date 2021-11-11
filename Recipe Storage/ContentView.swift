2f4a8eb9f9df92456127c3e899d6148304114f77
Tim Randall <trtimrandall@gmail.com>
2021-11-12 00:37:59 +1100

    got started need to make sure fr predicate works

--- Recipe Storage/ContentView.swift
+++ Recipe Storage/ContentView.swift
@@ -8,9 +8,50 @@
 import SwiftUI
 
 struct ContentView: View {
+    @Environment(\.managedObjectContext) var managedObjectContext
+    @FetchRequest(
+        entity: Recipe.entity(),
+        sortDescriptors: [NSSortDescriptor(keyPath: \Recipe.name, ascending: true )],
+        predicate: NSPredicate(format: "order == @%", 0))var items: FetchedResults<Recipe>
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        NavigationView {
+            ZStack{
+                Background()
+                VStack{
+                    HStack{
+                        Spacer()
+                        NavigationLink(destination: AddRecipeView()){
+                            Image(systemName: "plus")
+                                .resizable()
+                                .frame(width: 30, height: 30)
+                                .foregroundColor(Color.black)
+                                .padding()
+                        }
+                    }
+                    Spacer()
+                }
+            }
+        }//.navigationTitle("Recipe Storage")
+    }
+}
+
+struct RecipeView: View {
+    @Environment(\.managedObjectContext) var managedObjectContext
+    var body: some View {
+        Text("hello world")
+    }
+}
+
+struct AddRecipeView: View {
+    @Environment(\.managedObjectContext) var managedObjectContext
+    var body: some View {
+        Text("hello world")
+    }
+}
+
+struct Background: View {
+    var body: some View {
+        Color(.green)
     }
 }
 
--- Recipe Storage/Recipe_StorageApp.swift
+++ Recipe Storage/Recipe_StorageApp.swift
@@ -9,9 +9,10 @@ import SwiftUI
 
 @main
 struct Recipe_StorageApp: App {
+    let persistenceController = PersistenceController.shared
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            ContentView().environment(\.managedObjectContext, persistenceController.container.viewContext)
         }
     }
 }
