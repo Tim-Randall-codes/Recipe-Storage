87921349510317613f9e24a11e28fabe99401d42
Tim Randall <trtimrandall@gmail.com>
2021-11-11 23:16:12 +1100

    Initial Commit

---
+++ Recipe Storage/Recipe_StorageApp.swift
@@ -0,0 +1,17 @@
+//
+//  Recipe_StorageApp.swift
+//  Recipe Storage
+//
+//  Created by Tim Randall on 11/11/21.
+//
+
+import SwiftUI
+
+@main
+struct Recipe_StorageApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
