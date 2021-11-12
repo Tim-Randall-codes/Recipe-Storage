74f5cb7d9da26cfc1df69cc1ce12eb9ad45c7ab3
Tim Randall <trtimrandall@gmail.com>
2021-11-13 00:32:03 +1100

    found solution, need to finish

--- Recipe Storage/ContentView.swift
+++ Recipe Storage/ContentView.swift
@@ -92,23 +92,93 @@ struct AddRecipeView: View {
     @State var stepCounter: Int = 0
     @State var step1: String = ""
     @State var ing1: String = ""
+    @State var displayText: String = ""
+    @State var data = RecipeUnstored()
     var body: some View {
         ScrollView{
             ZStack{
                 Background()
                 VStack{
                     Text("\(name.word):").padding()
+                    Text(displayText).padding()
                     HStack{
                         TextField("Add step here", text: $step1)
+                        Button(action:{
+                            addStepToData()
+                        }, label:{
+                            ButtonWidget(words: "Add")
+                        })
                     }
                     Text(step1)
                 }
             }
         }
     }
-    func addData () {
+    func addStepToData () {
+        if stepCounter == 0 {
+            data.step1 = step1
+        }
+        else if stepCounter == 1 {
+            data.step2 = step1
+        }
+        else if stepCounter == 2 {
+            data.step3 = step1
+        }
+        else if stepCounter == 3 {
+            data.step4 = step1
+        }
+        else if stepCounter == 4 {
+            data.step5 = step1
+        }
+        else if stepCounter == 5 {
+            data.step6 = step1
+        }
+        else if stepCounter == 6 {
+            data.step7 = step1
+        }
+        else if stepCounter == 7 {
+            data.step8 = step1
+        }
+        else if stepCounter == 8 {
+            data.step9 = step1
+        }
+        else if stepCounter == 9 {
+            data.step10 = step1
+        }
+        else {
+            displayText = "Sorry you can only enter 10 steps"
+        }
+        stepCounter += 1
+    }
+    func addIngToData () {
         
     }
+}
+
+// save the user entries to the data object. Then have a button that saves the data object's data and name to a permanently stored recipe object
+
+struct RecipeUnstored {
+    var name: String = ""
+    var step1: String = ""
+    var step2: String = ""
+    var step3: String = ""
+    var step4: String = ""
+    var step5: String = ""
+    var step6: String = ""
+    var step7: String = ""
+    var step8: String = ""
+    var step9: String = ""
+    var step10: String = ""
+    var ing1: String = ""
+    var ing2: String = ""
+    var ing3: String = ""
+    var ing4: String = ""
+    var ing5: String = ""
+    var ing6: String = ""
+    var ing7: String = ""
+    var ing8: String = ""
+    var ing9: String = ""
+    var ing10: String = ""
 }
 
 struct Background: View {
